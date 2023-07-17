54bd50571332027a8e05091583590e6aec47b525
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-17 22:45:52 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -34,7 +34,7 @@ def open_pdf(bucket_name,dict_data):
         res = supabase.storage.from_(bucket_name).download(dict_data)
         with open('temp.pdf','wb') as f:
             f.write(res)
-            base64_pdf = base64.b64encode(f).decode('utf-8')        
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')        
             
         pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="480" height="720" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
