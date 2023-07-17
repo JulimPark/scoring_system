1fa62fc475ce2c3ba4908cb2c0677a9c5a673a5c
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-18 02:25:49 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -5,19 +5,19 @@ import pandas as pd
 
 
 # ## 온라인 게시용 수파 접속
-@st.cache_data
+@st.cache_resource
 def init_connection():
     url = st.secrets["supabase_url"]
     key = st.secrets["supabase_key"]
     return create_client(url, key)
 
-supabase = init_connection()
+supabase2 = init_connection()
 
 
 st.title('학습 :blue[컨텐츠]')
-response2= supabase.table('take_exam').select('*').execute()
+response2= supabase2.table('take_exam').select('*').execute()
 st.dataframe(respnse2)
-response1= supabase.table('exam_address').select('*').execute()
+response1= supabase2.table('exam_address').select('*').execute()
 st.dataframe(respnse1)
 st.markdown(f'<iframe src="https://docs.google.com/viewer?url={data_source}&embedded=true" width="100%" height="800px">', unsafe_allow_html=True)
         
