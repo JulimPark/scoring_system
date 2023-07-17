7154cd45830d28e79936aa431fd49965d7a2d4ba
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-17 21:56:30 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -5,7 +5,7 @@ import urllib
 from urllib.error import URLError, HTTPError
 import requests
 from pathlib import Path
-
+from io import BytesIO
 
 # ## 온라인 게시용 수파 접속
 @st.cache_resource
@@ -31,15 +31,25 @@ def open_pdf(url):
     try:
         # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',}
         headers = {'User-Agent':'Chrome/66.0.3359.181'}
-        req = urllib.request.Request(url,headers=headers)
+      
         
-        html = urllib.request.urlopen(req)
-        # st.markdown(html)
-        # st.write(html)
-        # req = requests.get(url,headers=headers,timeout=10)
-        # st.write(html)
+        # PDF 파일을 요청합니다.
+        response = requests.get(url)
+        
+        # PDF 파일을 BytesIO 객체에 저장합니다.
+        pdf_bytes = BytesIO(response.content)
+        
+        # # PDF 파일을 엽니다.
+        # pdf_file = open('pdf_file.pdf', 'wb')
+        
+        # # BytesIO 객체에서 PDF 파일에 데이터를 씁니다.
+        # pdf_file.write(pdf_bytes.getvalue())
+        
+        # # PDF 파일을 닫습니다.
+        # pdf_file.close()
+
         st.write(req.content)
-        base64_pdf = base64.b64encode(html.read()).decode('utf-8')        
+        base64_pdf = base64.b64encode(pdf_bytes).decode('utf-8')        
         pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="950" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
     except HTTPError as e:
