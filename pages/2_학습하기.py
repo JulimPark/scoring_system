a9c3b2c1d53cf6fc02d61037c8a847083fa6387c
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-17 23:14:20 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -34,7 +34,7 @@ def open_pdf(bucket_name,dict_data,url):
         
         base64_pdf = base64.b64encode(res).decode('utf-8')        
             
-        pdf_display = f'<iframe src={url} width="480" height="720"></iframe>'
+        pdf_display = f'<a href="{url}"></a>'
         # pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="480" height="720" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
         
