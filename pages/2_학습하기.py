f31f979f62c6ebf631a29768a5eef0b9e331fbc6
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-17 23:30:10 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -26,21 +26,21 @@ def view_data(bucket_name):
 
 def open_pdf(bucket_name,dict_data,url):
     # try:
-        # pdf_buffer = BytesIO()
-        # res = supabase.storage.from_(bucket_name).download(dict_data)
-        # pdf_buffer.write(res)
-        
+        pdf_buffer = BytesIO()
         res = supabase.storage.from_(bucket_name).download(dict_data)
+        pdf_buffer.write(res)
         
-        base64_pdf = base64.b64encode(res).decode('utf-8')        
+        # res = supabase.storage.from_(bucket_name).download(dict_data)
+        
+        # base64_pdf = base64.b64encode(res).decode('utf-8')        
 
         
         # st.markdown(f'<embed src="https://drive.google.com/viewerng/viewer?embedded=true&url={url}" width="400" height="400">', unsafe_allow_html=True)
     
         # pdf_display = f'<a href="{url}"></a>'
-        pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="480" height="720" type="application/pdf" scrolling="yes"></iframe>'
-        st.markdown(pdf_display, unsafe_allow_html=True)
-        st.image(res)
+        # pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="480" height="720" type="application/pdf" scrolling="yes"></iframe>'
+        # st.markdown(pdf_display, unsafe_allow_html=True)
+        st.image(pdf_buffer)
     # except HTTPError as e:
     #     err = e.read()
     #     code = e.getcode()
