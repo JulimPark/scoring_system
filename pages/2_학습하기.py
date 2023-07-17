81196712489316bf9dd689efc93c53d5ffc6f44e
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-18 02:10:58 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -18,19 +18,4 @@ st.title('학습 :blue[컨텐츠]')
 response1= supabase.table('exam_address').select('*').eq('학년','고3/재수').execute()
 st.dataframe(respnse1)
 st.markdown(f'<iframe src="https://docs.google.com/viewer?url={data_source}&embedded=true" width="100%" height="800px">', unsafe_allow_html=True)
-        open_pdf(bucket_name,dict_data,data_source)
-
-def open_pdf(bucket_name,dict_data,url):
-    
-        res = supabase.storage.from_(bucket_name).download(dict_data)
         
-        base64_pdf = base64.b64encode(res).decode('utf-8')        
-
-        
-        st.markdown(f'<iframe src="https://docs.google.com/viewer?url={url}&embedded=true" width="100%" height="800px">', unsafe_allow_html=True)
-        # st.markdown(f'<iframe src="{url}/preview#toolbar=0" width="100%" height="800px">', unsafe_allow_html=True)
-    
-
-    
-
-view_data('testbuck')
