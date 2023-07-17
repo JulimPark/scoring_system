6f66a4783f284881b90b5887397849bb3b4a7b25
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-18 03:12:12 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -17,17 +17,21 @@ supabase2 = init_connection()
 
 st.title('학습 :blue[컨텐츠]')
 def set_mark(curri):
-    resource = supabase2.table('exam_address').select('*').eq('학년',curri).execute()
-    df = pd.DataFrame(resource.data)
-    
-    add_list = df.주소.to_list()
-    content_list = df.내용.to_list()
-    
-    for i in range(len(add_list)):
-        pdf_display = F'<iframe src="{add_list[i]}" width="100%" height="800px"></iframe>'
-        with st.expander(content_list[i]):
-            st.subheader(content_list[i])
-            st.markdown(pdf_display, unsafe_allow_html=True)
+    try:
+        resource = supabase2.table('exam_address').select('*').eq('학년',curri).execute()
+        df = pd.DataFrame(resource.data)
+        
+        add_list = df.주소.to_list()
+        content_list = df.내용.to_list()
+        
+        for i in range(len(add_list)):
+            pdf_display = F'<iframe src="{add_list[i]}" width="100%" height="800px"></iframe>'
+            with st.expander(content_list[i]):
+                st.subheader(f'{content_list[i]} - {curri}')
+                st.markdown(pdf_display, unsafe_allow_html=True)
+    except:
+        with st.st.expander(curri):
+            st.write(f'{curri}에 대한 컨텐츠가 없습니다.')
 
 set_mark('고3/재수')
 
