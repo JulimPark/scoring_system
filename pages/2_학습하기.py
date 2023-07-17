f7adcb0a40466a62adeec98c8a2ecab832317161
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-17 22:13:34 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -29,25 +29,19 @@ def view_data(bucket_name):
 
 def open_pdf(url):
     try:
-        # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',}
-        headers = {'User-Agent':'Chrome/66.0.3359.181'}
-      
+                
         
-        # PDF 파일을 요청합니다.
+        # PDF를 읽습니다.
         response = requests.get(url)
+        pdf_bytes = response.content
         
-        # PDF 파일을 BytesIO 객체에 저장합니다.
-        pdf_bytes = BytesIO(response.content)
-        
-        # PDF 파일을 엽니다.
-        pdf_file = open('pdf_file.pdf', 'wb')
-        
-        # BytesIO 객체에서 PDF 파일에 데이터를 씁니다.
-        pdf_file.write(pdf_bytes.getvalue())
+        # PDF를 BytesIO 객체에 저장합니다.
+        pdf_buffer = BytesIO()
+        pdf_buffer.write(pdf_bytes)
         
 
         # st.write(req.content)
-        base64_pdf = base64.b64encode(pdf_file).decode('utf-8')        
+        base64_pdf = base64.b64encode(pdf_buffer.getvalue()).decode('utf-8')        
         pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="950" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
         
