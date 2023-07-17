188116c6e8c88fcf3772de8b6ba57beef955f6e7
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-18 02:22:36 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -15,6 +15,8 @@ supabase = init_connection()
 
 
 st.title('학습 :blue[컨텐츠]')
+response2= supabase.table('take_exam').select('*').execute()
+st.dataframe(respnse2)
 response1= supabase.table('exam_address').select('*').execute()
 st.dataframe(respnse1)
 st.markdown(f'<iframe src="https://docs.google.com/viewer?url={data_source}&embedded=true" width="100%" height="800px">', unsafe_allow_html=True)
