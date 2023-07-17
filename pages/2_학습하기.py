41867ca58c481f8d3c4fac8e8bb979945ed0266b
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-17 21:12:37 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -38,7 +38,7 @@ def open_pdf(url):
         # st.write(html)
         req = requests.get(url)#,headers=headers)
         
-        base64_pdf = base64.b64encode(req.content).decode('utf-8')        
+        base64_pdf = base64.b64encode(req.raw).decode('utf-8')        
         pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="950" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
     except HTTPError as e:
