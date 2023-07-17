70319d5abcbfddbea28d2eddb98f55b568848079
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-17 18:28:10 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -20,10 +20,10 @@ supabase = init_connection()
 st.title('학습 :blue[컨텐츠]')
 def view_data(bucket_name):
     res = supabase.storage.from_(bucket_name).list()
-    st.write(res)
     for i in range(len(res)):
         dict_data = res[i]['name']
         data_source = supabase.storage.from_(bucket_name).get_public_url(dict_data)
+        st.write(data_source)
         with st.expander(dict_data[:-4]):
             st.header(dict_data[:-4])
             open_pdf(data_source)
@@ -33,6 +33,7 @@ def open_pdf(url):
         headers = {'User-Agent':'Chrome/66.0.3359.181'}
         req = urllib.request.Request(url,headers=headers)
         html = urllib.request.urlopen(req)
+        st.write(html)
         base64_pdf = base64.b64encode(html.read()).decode('utf-8')        
         pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="950" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
