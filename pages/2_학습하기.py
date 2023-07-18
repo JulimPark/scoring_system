15a47976d14999e97babcca2e852fa41009a3bd9
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-07-18 14:48:47 +0900

    Update 2_학습하기.py

--- pages/2_학습하기.py
+++ pages/2_학습하기.py
@@ -14,6 +14,12 @@ def init_connection():
 supabase2 = init_connection()
 
 
+end_color = st.select_slider(
+    '학습 범위를 선택하세요',
+    options=['고등선행', '고1','고2','고3/재수'],
+    value='고1')
+st.write('You selected wavelengths', end_color)
+
 
 st.title('학습 :blue[컨텐츠]')
 def set_mark(curri):
@@ -33,6 +39,13 @@ def set_mark(curri):
         with st.expander(curri):
             st.write(f'{curri}에 대한 컨텐츠가 없습니다.')
 
-set_mark('고3/재수')
-
-set_mark('고1')
+if end_color == '고등선행':
+    set_mark('고3/재수')
+elif end_color == '고1':
+    set_mark('고1')
+elif end_color == '고2':
+    set_mark('고2')
+elif end_color == '고3/재수':
+    set_mark('고3/재수')
+else:
+    st.write('선택 범위의 자료가 없습니다.')
